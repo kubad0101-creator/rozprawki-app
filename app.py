cb2371f86ad61c1c4fbbecdf2661a6d8eac2e7f2
kubad0101-creator <kubad0101@gmail.com>
2026-07-12 17:06:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.secret_key = "Open196!_System_Rozprawek_2024"
 
 # --- KONFIGURACJA BREVO API ---
 BREVO_API_KEY = "xsmtpsib-63c5b87db079307d7d8d7aafeebc34301b3fe262ac8116adb1f7f2a32cf01a6b-rLF2ev2QrXr1g4Hd" 
-BREVO_SENDER_EMAIL = "TWÓJ_EMAIL_Z_KOTA_BREVO@gmail.com" # <--- TUTAJ WPISZ SWÓJ EMAIL Z BREVO!
+BREVO_SENDER_EMAIL = "b1bb47001@smtp-brevo.com" # <--- TUTAJ WPISZ SWÓJ EMAIL Z BREVO!
 # ------------------------------
 
 UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
